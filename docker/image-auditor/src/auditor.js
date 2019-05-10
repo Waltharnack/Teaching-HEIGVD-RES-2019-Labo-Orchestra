ee8dbec802b760d8e504cfd0b7c99c35004870fa
Simon Walther <simon.walt@hotmail.com>
2019-05-10 22:25:14 +0200

    implement auditor

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -1,16 +1,52 @@
 // UDP Node.js dependency
 const dgram = require('dgram');
 
+// moment
+const moment = require('moment');
+
+// tcp
+const net = require('net');
+
 // protocol for auditor
 const protocol = require('./auditor-protocol');
 
 // datagram socket
 const socket = dgram.createSocket('udp4');
 
+// list of musicians
+let musicians = new Map();
+
+// tcp server creation
+const tcpServer = net.createServer();
+
+// listen for connection requests
+tcpServer.listen(protocol.PROTOCOL_PORT, () => {
+  console.log(`listening on port ${protocol.PROTOCOL_PORT}`);
+});
+
+// do that on connection
+tcpServer.on('connection', (tcpSocket) => {
+  const payload = [];
+
+  musicians.forEach((value, key) => {
+    if (moment.duration(moment().diff(moment(value[1]))).seconds() >= 5) {
+      musicians.delete(key);
+    } else {
+      payload.push({ uuid: key, instrument: value[0], activeSince: value[1] });
+    }
+  });
+
+  tcpSocket.write(JSON.stringify(payload));
+});
+
+// listen to musicians on multicast address
 socket.bind(protocol.PROTOCOL_PORT, () => {
   socket.addMembership(protocol.PROTOCOL_MULTICAST_ADDRESS);
 });
 
 socket.on('message', (msg, rinfo) => {
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  const jsonMessage = JSON.parse(msg);
+  const activeSince = moment().toISOString();
+  musicians.set(jsonMessage.uuid, [jsonMessage.instrument, activeSince]);
+  console.log(musicians);
 });
